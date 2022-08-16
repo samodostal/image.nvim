1030335aa768e01867512a7e9cdf70774ecd5a50
samodostal <samuel.dostal.sk@gmail.com>
2022-08-16 20:06:04 +0200

    feat: center image in the buffer

--- lua/image/init.lua
+++ lua/image/init.lua
@@ -14,13 +14,17 @@ local on_image_open = function()
 	local buf_id = 0
 	local buf_path = vim.api.nvim_buf_get_name(buf_id)
 
-	local ascii_width, ascii_height = dimensions.calculate_ascii_width_height(buf_id, buf_path, global_opts)
+	local ascii_width, ascii_height, horizontal_padding, vertical_padding = dimensions.calculate_ascii_width_height(
+		buf_id,
+		buf_path,
+		global_opts
+	)
 
 	options.set_options_before_render(buf_id)
 	ui.buf_clear(buf_id)
 
 	local ascii_data = api.get_ascii_data(buf_path, ascii_width, ascii_height, global_opts)
-	ui.buf_insert_data(buf_id, ascii_data)
+	ui.buf_insert_data_with_padding(buf_id, ascii_data, horizontal_padding, vertical_padding)
 
 	options.set_options_after_render(buf_id)
 end
--- lua/image/ui/init.lua
+++ lua/image/ui/init.lua
@@ -4,8 +4,24 @@ M.buf_clear = function(buf_id)
 	vim.api.nvim_buf_set_lines(buf_id, 0, -1, true, { "" })
 end
 
-M.buf_insert_data = function(buf_id, data)
-	vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, data)
+M.buf_insert_data_with_padding = function(buf_id, data, horizontal_padding, vertical_padding)
+	local horizontal_string = string.rep(" ", horizontal_padding)
+
+	local processed_data = {}
+
+	for i = 1, vertical_padding do
+		table.insert(processed_data, "")
+	end
+
+	for _, line in ipairs(data) do
+		table.insert(processed_data, horizontal_string .. line)
+	end
+
+	for i = 1, vertical_padding do
+		table.insert(processed_data, "")
+	end
+
+	vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, processed_data)
 end
 
 return M
